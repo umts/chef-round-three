30237eea4cd02df03b44c47ba20b6e66226a1948
Matt Moretti <werebus@gmail.com>
2013-07-02 18:33:23 -0400

    Get our round-three shibboleth recipe in-line with the current cookbook
    Also, added the attribute needed for umts/round-three#29

--- recipes/shibboleth.rb
+++ recipes/shibboleth.rb
@@ -3,3 +3,8 @@ include_recipe "shibboleth"
 web_app "round-three-shib" do
   template "round-three-shib.conf.erb"
 end
+
+cookbook_file '/etc/shibboleth/attributes.d/umass.xml' do
+  mode '0644'
+  notifies :create, 'ruby_block[build-attribute-map]'
+end
