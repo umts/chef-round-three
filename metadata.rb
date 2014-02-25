c50eafc52b0d0307a08cab26ee8552e87c20f588
Matt Moretti <werebus@gmail.com>
2014-02-25 17:10:43 -0500

    Testing skeleton

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,8 @@ maintainer_email "transit-mis@admin.umass.edu"
 license          "All rights reserved"
 description      "Installs/Configures round-three"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.20"
+version          "0.1.0"
+name             "round-three"
 
 %w{ apache2 application application_ruby git database postgresql rbenv rsnapshot shibboleth ssl users sudo xml }.each do |cb|
   depends cb
