3f26cb4d71b3cc8196ebaa3f7f63e34577ad091e
Matt Moretti <werebus@gmail.com>
2015-06-11 16:03:43 -0400

    Maybe we'll use the application cookbook after all?

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,7 @@ version          '1.0.0'
 depends 'ruby-install', '~> 0.2.0'
 depends 'apache2', '~> 3.0.1'
 depends 'passenger', '~> 0.1.0'
+depends 'application', '~> 4.1.6'
 
 supports 'ubuntu'
 supports 'centos', '>= 7'
