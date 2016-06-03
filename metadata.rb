e0d9ba3698e61f32b1b1da46bb68fc86f37300d5
Matt Moretti <werebus@gmail.com>
2016-06-03 13:44:17 -0400

    Manditory name

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "transit-mis@admin.umass.edu"
 license          "All rights reserved"
 description      "Installs/Configures round-three"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name             "round-three"
 version          "0.0.21"
 
 %w{ apache2 application application_ruby git database postgresql rbenv rsnapshot shibboleth ssl users sudo xml }.each do |cb|
